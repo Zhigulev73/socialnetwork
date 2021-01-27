c55eb250ff4ea6ef63ba20537e717d760b81c06c
Zh1gul <kzhigulev2015@mail.ru>
2021-01-27 16:12:56 +0400

    do pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,81 +1,77 @@
 import React from 'react'
 import styles from './users.module.css'
+import * as axios from "axios";
+import userPhoto from '../../assets/274px-Александр_Невский_2013.jpg'
 
-let Users = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0 ) {
-        props.setUsers(
-            [{
-                id: 1,
-                photoUrl: 'https://icdn.lenta.ru/images/2018/01/25/14/20180125140445266/pic_2100e8de52f00d949f474f53a9bfb99b.jpg',
-                followed: false,
-                fullName: 'Kirill',
-                status: 'Im a boss',
-                location: 'Ulyanovsk',
-                country: 'Russia'
-            },
-                {
-                    id: 2,
-                    photoUrl: 'https://icdn.lenta.ru/images/2018/01/25/14/20180125140445266/pic_2100e8de52f00d949f474f53a9bfb99b.jpg',
-                    followed: false,
-                    fullName: 'Dasha',
-                    status: 'Im a boss',
-                    location: 'Ulyanovsk',
-                    country: 'Russia'
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://icdn.lenta.ru/images/2018/01/25/14/20180125140445266/pic_2100e8de52f00d949f474f53a9bfb99b.jpg',
-                    followed: false,
-                    fullName: 'Ilya',
-                    status: 'Im a boss',
-                    location: 'Ulyanovsk',
-                    country: 'Russia'
-                },
-                {
-                    id: 4,
-                    photoUrl: 'https://icdn.lenta.ru/images/2018/01/25/14/20180125140445266/pic_2100e8de52f00d949f474f53a9bfb99b.jpg',
-                    followed: true,
-                    fullName: 'Den',
-                    status: 'Im a boss',
-                    location: 'Ulyanovsk',
-                    country: 'Russia'
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                    this.props.setUsers(response.data.items)
+                    this.props.setTotalUsersCount(response.data.totalCount)
                 }
-                ]
-        )
+            )
     }
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                    this.props.setUsers(response.data.items)
+                })
+    }
+
+    render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+
+        let pages = []
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
+        return <div>
+            <div>
+                {pages.map(p => {
+                    return <span className={this.props.currentPage === p && styles.selectedPage}
+                                 onClick={() => {
+                                    this.onPageChanged(p)
+                                 }}>{p}</span>
+                })}
+            </div>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
 
                     </div>
                 </span>
-                <span>
                     <span>
-                        <div>{u.fullName}</div>
+                    <span>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
+
 
 export default Users
