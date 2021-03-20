d61f90e0c8642143a96a77ee6061e87ac81c5075
Zh1gul <kzhigulev2015@mail.ru>
2021-03-20 20:04:12 +0400

    add classnames pack

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {Route, withRouter} from "react-router-dom";
+import {Redirect, Route, Switch, withRouter} from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
@@ -31,18 +31,26 @@ class App extends React.Component {
                 <HeaderContainer/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs'
-                           render={withSuspense(DialogsContainer)}/>
+                    <Switch>
+                        <Route exact path='/'
+                               render={() => <Redirect to={'/profile'} />}/>
 
-                    <Route path='/profile/:userId?'
-                           render={withSuspense(ProfileContainer)}
-                           />
+                        <Route path='/dialogs'
+                               render={withSuspense(DialogsContainer)}/>
 
-                    <Route path='/users'
-                           render={() => <UsersContainer/>}/>
+                        <Route path='/profile/:userId?'
+                               render={withSuspense(ProfileContainer)}
+                        />
 
-                    <Route path='/login'
-                           render={() => <Login/>}/>
+                        <Route path='/users'
+                               render={() => <UsersContainer/>}/>
+
+                        <Route path='/login'
+                               render={() => <Login/>}/>
+
+                        <Route path='*'
+                               render={() => <div>404 NOT FOUND</div>}/>
+                    </Switch>
                 </div>
             </div>
         )
